44c98adc26ef0446ee90ce053ed53af3dd06eeac
luciano.fagundes <luciano.fagundes@softplan.com.br>
2020-08-19 00:25:48 -0300

    second commit

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,39 @@
+class CalcController {
+
+    constructor(){
+
+        this._displayCalc = "0"
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+
+        let displayCalEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#data")
+        let timeEl = document.querySelector("#hora")
+
+        displayCalEl.innerHTML = '4567'
+        dateEl.innerHTML = '19/08/2020'
+        timeEl.innerHTML = '00:15'
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
+
+
+}
